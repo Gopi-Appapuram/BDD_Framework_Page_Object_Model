695911abe91b4041f797d4a416bb5a258aad5a39
Gopi_Appapuram <appapuramgopi6@gmail.com>
2024-05-31 10:38:11 +0530

    excel data

--- src/test/java/Hooks/hooks.java
+++ src/test/java/Hooks/hooks.java
@@ -1,21 +1,13 @@
 package Hooks;
 
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import io.cucumber.java.After;
+
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.testng.annotations.AfterTest;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import static UtilityClasses.WebDriverManager.driver;
 
@@ -24,18 +16,18 @@ public class hooks {
 
     @AfterStep
     public void takeScreenShot(Scenario scenario) {
-        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(screenshot, "image/png", scenario.getName());
-
-        if (scenario.isFailed()) {
-            screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName().replace(" ", "")+"failed.png");
+        if(!scenario.isFailed()){
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName().replace(" ", "") + "_passed.png");
+        } else if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName().replace(" ", "") + "_failed.png");
         }
     }
 
     @AfterAll
-    public static void closeBrowser(){
-        if(driver != null){
+    public static void closeBrowser() {
+        if (driver != null) {
             driver.close();
             driver.quit();
         }
